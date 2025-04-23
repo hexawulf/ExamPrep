680ca361c595eda74ef3cf753f13c6ee9cbb592a
0xWulf <0xwulf@proton.me>
2025-04-23 09:07:39 +0800

    Revised Q01

--- src/abacusAI2/Q01.java
+++ src/abacusAI2/Q01.java
@@ -1,4 +1,8 @@
 package abacusAI2;
+//1. Create a Simple Java Class
+//Reference: Section 3.2, p. 41
+//Write a Java class named Book with no attributes or methods.
+//Target concept: Declaring a class
 
 public class Q01
 {
